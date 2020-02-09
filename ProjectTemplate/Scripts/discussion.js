6a86988fad1071f1617d60bd4e3539a49a384ffd
Jared Gonzales <jgonz154@asu.edu>
2020-02-08 22:56:09 -0700

    added login and discussion UI

---
+++ ProjectTemplate/Scripts/discussion.js
@@ -0,0 +1,43 @@
+﻿var discussion = (function () {
+    var submit; 
+
+    submit = function () {
+        const url = 'ProjectServices.asmx/InsertPost'; 
+        const postText = $('#txtPost').val(); 
+
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: JSON.stringify({
+                post: postText
+            }),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (data) {
+                $('#postmodal').modal('hide');
+                $(".card-container").html('<div class="card">' +
+                    '<div class="card-block">' + 
+                    '<h1 class="card-text square-card float-left">SD</h1>' +
+                    '<h4 class="card-title">Employee Post</h4>' +
+                    '<p class="card-text">' + postText + '</p>' + 
+                    '</div>' + 
+                    '<div class="card-footer">' + 
+                    '<p class="card-text text-right">Reply</p>' + 
+                    '</div>'
+                    )
+            },
+            error: function (err) {
+
+            }
+        });
+    }
+
+
+    return {
+        submit: submit
+    }
+})(); 
+
+$("#postmodal").on('hide.bs.modal', function () {
+    $('#txtPost').val(''); 
+});
---
+++ ProjectTemplate/Scripts/login.js
@@ -0,0 +1,50 @@
+﻿var login = (function () {
+    var userLogin,
+        displayError;
+
+    userLogin = function () {
+        const url = 'ProjectServices.asmx/ValidateUser'; 
+        const username = $('#username').val(); 
+        const password = $('#password').val(); 
+
+        if (username === '' ||
+            password === '') {
+            displayError('Please enter Username and Password.');
+            return false; 
+        }
+
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: JSON.stringify({
+                username: username,
+                password: password
+            }), 
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (data) {
+                if (data.d === 'Success!') {
+                    window.open('Discussion.html', '_self'); 
+                } else {
+                    displayError('Incorect Username or Password.');
+                }
+            },
+            error: function (err) {
+                displayError('Unexpected error occurred, please try again later.');
+            }
+        });
+    }
+
+    displayError = function (msg) {
+        $(".error-container").show();
+        $(".error-container").html('');
+        $(".error-container").html('<p>&#8226; ' + msg + '</p>');
+        $(".logincontainer").css({ "height": 325 });
+    }
+
+    return {
+        userLogin: userLogin,
+        displayError: displayError
+    }
+
+})();
